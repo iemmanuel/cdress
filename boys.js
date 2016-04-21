cb911a5d7bc0e3e720ea253b6b4fbfd0e8101f9d
iemmanuel <iemmanuel@cristoreyny.org>
2016-04-21 12:59:46 +0000

    added shirt

---
+++ boys.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+   
+
+
+var shirts = ["shirt1.jpg","shirt2.jpg","shirt3.jpg","shirt4.jpg","shirt5.jpg"];
+
+    $("#row1").append("<img src = \"shirts/" + shirts[1]+"\">");
+    
+    
+});
+
